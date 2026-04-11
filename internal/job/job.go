5157f3ec5b2b21cfafefd29930f84b089a09b19e
Aryan Singh Thakur <aryana123b456@gmail.com>
2026-04-11 08:08:01 +0530

    Define job struct and its diffrent status

---
+++ internal/job/job.go
@@ -0,0 +1,31 @@
+package job
+
+import (
+	"encoding/json"
+	"time"
+)
+
+type Status string
+
+const (
+	StatusPending Status = "PENDING"
+	StatusRunning Status = "RUNNING"
+	StatusDone    Status = "DONE"
+	StatusFailed  Status = "Failed"
+	StatusDead    Status = "DEAD"
+)
+
+type Job struct {
+	ID         string			`json:"id"`
+	Type       string			`json:"type"`
+	Payload    json.RawMessage	`json:"payload"`
+	Status     Status			`json:"status"`
+	Priority   int				`json:"priority"`
+	Delay      time.Duration	`json:"delay"`
+	MaxRetries int				`json:"max_retries"`
+	RetryCount int				`json:"retry_count"`
+	Error      string			`json:"error,omitempty"`
+	CreatedAt  time.Time		`json:"created_at"`
+	UpdatedAt  time.Time		`json:"updated_at"`
+	RunAfter   time.Time		`json:"run_after"`
+}
