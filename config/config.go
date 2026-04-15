7fb3ee1199b42fc454212237bc88c510081dc022
Aryan Singh Thakur <aryana123b456@gmail.com>
2026-04-15 20:44:39 +0530

    Add config loading from env

--- config/config.go
+++ config/config.go
@@ -1,9 +1,36 @@
 package config
 
+import(
+	"os"
+	"strconv"
+)
+
 type Config struct{
 	Port string
 	MaxRetries int
 	BaseDelay int
 	MaxDelay int
 	NumWorkers int
+}
+
+func envToInt(v string, defaultValue int) int {
+	if v != "" {
+		val, err := strconv.Atoi(v)
+		if err == nil {
+			return val
+		}
+	}
+	return defaultValue
+}
+
+func LoadConfig() *Config {
+	// Load from env or file
+	return &Config{
+		Port: os.Getenv("PORT"),
+
+		MaxRetries: envToInt(os.Getenv("MAX_RETRIES"), 5),
+		BaseDelay: envToInt(os.Getenv("BASE_DELAY"), 100),
+		MaxDelay: envToInt(os.Getenv("MAX_DELAY"), 5000),
+		NumWorkers: envToInt(os.Getenv("NUM_WORKERS"), 10),
+	}
 }
