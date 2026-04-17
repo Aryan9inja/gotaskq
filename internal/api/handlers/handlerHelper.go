18ba367b7f90e91519a3e858fd11a69dedccde18
Aryan Singh Thakur <aryana123b456@gmail.com>
2026-04-17 21:40:38 +0530

    Add handlerHelper.go with JSON response utilities and Handler struct

---
+++ internal/api/handlers/handlerHelper.go
@@ -0,0 +1,36 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/Aryan9inja/gotaskq/internal/job"
+	"github.com/Aryan9inja/gotaskq/internal/queue"
+	"github.com/Aryan9inja/gotaskq/pkg/snowflake"
+)
+
+type Handler struct {
+	store        job.Store
+	queueManager *queue.QueueManager
+	idGenerator  *snowflake.Snowflake
+}
+
+func New(st job.Store, qm *queue.QueueManager, idGen *snowflake.Snowflake) *Handler {
+	return &Handler{
+		store:        st,
+		queueManager: qm,
+		idGenerator:  idGen,
+	}
+}
+
+func WriteJSON(w http.ResponseWriter, status int, v any) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	_ = json.NewEncoder(w).Encode(v)
+}
+
+func WriteError(w http.ResponseWriter, status int, msg string) {
+	WriteJSON(w, status, map[string]string{
+		"error": msg,
+	})
+}
