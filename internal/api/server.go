e0e3d774424b5e94a4cfa5bb5cb3c49dc84d5b50
Aryan Singh Thakur <aryana123b456@gmail.com>
2026-04-26 21:08:26 +0530

    Add dlqStore to Server - and update NewServer constructor

--- internal/api/server.go
+++ internal/api/server.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/Aryan9inja/gotaskq/internal/api/handlers"
+	"github.com/Aryan9inja/gotaskq/internal/dlq"
 	"github.com/Aryan9inja/gotaskq/internal/job"
 	"github.com/Aryan9inja/gotaskq/internal/queue"
 	"github.com/Aryan9inja/gotaskq/pkg/snowflake"
@@ -14,23 +15,28 @@ type Server struct{
 	store job.Store
 	queueManager *queue.QueueManager
 	idGenerator *snowflake.Snowflake
+	dlqStore dlq.DlqInterface
 }
 
-func NewServer(st job.Store, qm *queue.QueueManager, idGen *snowflake.Snowflake) *Server{
+func NewServer(st job.Store, qm *queue.QueueManager, idGen *snowflake.Snowflake, dlqStore dlq.DlqInterface) *Server{
 	return &Server{
 		store: st,
 		queueManager: qm,
 		idGenerator: idGen,
+		dlqStore: dlqStore,
 	}
 }
 
 func (s *Server) Start(addr string) error{
 	r := chi.NewRouter()
-	h := handlers.New(s.store, s.queueManager, s.idGenerator)
+	h := handlers.New(s.store, s.queueManager, s.idGenerator, s.dlqStore)
 
 	// Route definition here
 	r.Post("/jobs", h.CreateJob)
 	r.Get("/jobs/{id}", h.GetJob)
+	r.Get("/dlq", h.ListDeadJobs)
+	r.Post("/dlq/{id}/replay", h.ReplayDeadJob)
+	r.Delete("/dlq/{id}", h.DeleteDeadJob)
 
 	return http.ListenAndServe(addr, r)
 }
