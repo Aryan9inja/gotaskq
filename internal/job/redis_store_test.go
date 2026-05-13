e107d1c174be9af21de76986621d0303f62c4140
Aryan Singh Thakur <aryana123b456@gmail.com>
2026-05-13 17:41:19 +0530

    Add some tests for Redis store functionality - Saving and Getting JOB - Updating JOB status

---
+++ internal/job/redis_store_test.go
@@ -0,0 +1,60 @@
+package job_test
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/Aryan9inja/gotaskq/internal/job"
+	testutils "github.com/Aryan9inja/gotaskq/internal/testUtils"
+)
+
+func TestRedisStore(t *testing.T){
+	client := testutils.GetRedisClient(t)
+	defer client.Close()
+
+	ctx := context.Background()
+	ttl := 1*time.Hour
+
+	t.Run("Save and Get", func(t *testing.T) {
+		testutils.ClearRedis(t, client)
+		store, _ := job.NewRedisStore(client, ttl)
+
+		j1 := testutils.NewTestJob("j1", 5, 0)
+		if err := store.Save(ctx, j1); err!=nil{
+			t.Fatalf("Save failed: %v", err)
+		}
+
+		got, err := store.Get(ctx, j1.ID)
+		if err != nil {
+			t.Fatalf("Get failed: %v", err)
+		}
+
+		if got.ID != j1.ID || got.Priority!=j1.Priority || got.Type != j1.Type{
+			t.Errorf("Job Mismatch, expected %+v, got %+v",j1, got)
+		}
+	})
+
+	t.Run("Update Status", func(t *testing.T) {
+		testutils.ClearRedis(t, client)
+		store, _ := job.NewRedisStore(client, ttl)
+
+		j1 := testutils.NewTestJob("j1", 5, 0)
+		store.Save(ctx,j1)
+
+		// Valid Transition
+		if err := store.UpdateStatus(ctx, "j1", job.StatusRunning); err!=nil{
+			t.Errorf("Expected valid transition to succeed, got error: %v", err)
+		}
+
+		got, _ := store.Get(ctx, "j1")
+		if got.Status != job.StatusRunning{
+			t.Errorf("Expected status to be RUNNING, got %s", got.Status)
+		}
+
+		err := store.UpdateStatus(ctx, "j2", job.StatusPending)
+		if err == nil {
+			t.Error("Expected error for invalid transition")
+		}
+	})
+}
