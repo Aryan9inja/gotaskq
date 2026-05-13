8a046e821aee541ed3684e54267e792450d7b130
Aryan Singh Thakur <aryana123b456@gmail.com>
2026-05-13 17:41:40 +0530

    Add function for creating test job instances

---
+++ internal/testUtils/jobs.go
@@ -0,0 +1,26 @@
+package testutils
+
+import (
+	"encoding/json"
+	"time"
+
+	"github.com/Aryan9inja/gotaskq/internal/job"
+)
+
+func NewTestJob(id string, priority int, delay time.Duration) *job.Job {
+	now := time.Now()
+
+	payload, _ := json.Marshal(map[string]any{"data": "test"})
+	return &job.Job{
+		ID:         id,
+		Type:       "test",
+		Payload:    payload,
+		Priority:   priority,
+		CreatedAt:  now,
+		Delay:      delay,
+		RunAfter:   now.Add(delay),
+		RetryCount: 0,
+		MaxRetries: 3,
+		Status:     job.StatusPending,
+	}
+}
