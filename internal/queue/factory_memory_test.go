17816e8e0af5d0cb13369bdd7fe6e4245164e3e4
Aryan Singh Thakur <aryana123b456@gmail.com>
2026-05-15 17:48:30 +0530

    Add tests for memory queue factory

---
+++ internal/queue/factory_memory_test.go
@@ -0,0 +1,29 @@
+package queue
+
+import (
+	"testing"
+)
+
+func TestMemoryFactory(t *testing.T) {
+	factory := NewMemoryFactory()
+
+	t.Run("Create Success", func(t *testing.T) {
+		q, err := factory.New("test-q")
+		if err != nil {
+			t.Fatalf("Expected success, got error : %v", err)
+		}
+		if q.Name() != "test-q" {
+			t.Fatalf("Expected queue name 'test-q', got '%s'", q.Name())
+		}
+		if _, ok := q.(*MemoryQueue); !ok {
+			t.Fatalf("Expected type *MemoryQueue, got %T", q)
+		}
+	})
+
+	t.Run("Empty Name", func(t *testing.T) {
+		_, err := factory.New(" ")
+		if err!=ErrEmptyQueueName{
+			t.Errorf("Expected ErrEmptyQueueName, got %v", err)
+		}
+	})
+}
